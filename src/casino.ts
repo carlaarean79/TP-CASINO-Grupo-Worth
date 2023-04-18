002545d56c8a98366aa38076a0b40608ae3043f5
Fulviacón <fulviacon@gmail.com>
2023-04-18 20:11:17 -0300

    implementé la clase dados

--- src/casino.ts
+++ src/casino.ts
@@ -5,14 +5,14 @@ import { Tragamonedas } from './Tragamonedas';
 //el cual está compuesto por las diferentes salas de juegos, el nombre del casino y la cant de salas 
 //que lo componen
 export class Casino {//clase individual//inicio de la clase
-    private nombre: string;//miembros datos o variables internas//declaramos las variables
+    private nombre: string; //miembros datos o variables internas//declaramos las variables
     private cantSalas: number;//private es un modificador de acceso
     private sala1: Tragamonedas;//composicion
     private sala2: Dados;//composicion
     private sala3: Ruleta;//composicion
     
      public constructor(nombre:string,cantSalas:number,sala1:Tragamonedas,sala2:Dados,sala3:Ruleta){//comienza constructor
-        this.nombre=nombre;//constructor me sirve para crear las instancias de los 
+        this.nombre=nombre; //constructor me sirve para crear las instancias de los 
         this.cantSalas=cantSalas;//nuevos objetos
         this.sala1=sala1;//inicializamos variables
         this.sala2 =sala2;//se le asigna el parámetro correspondiente
